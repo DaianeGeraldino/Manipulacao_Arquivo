579fade1369c521b723269877acaa78434944e10
Daiane Geraldino <129515871+DaianeGeraldino@users.noreply.github.com>
2023-10-09 18:08:06 -0300

    Update exercicio2.py

--- lista de exercicio/exercicio2.py
+++ lista de exercicio/exercicio2.py
@@ -1,10 +1,21 @@
-arquivo_usuario = 'usuarios.txt'
+arquivo_estudantes = 'usuarios.txt'
 
 dados_lidos = {}
+estudante_atual = None
 
-with open(arquivo_usuario, 'r') as arquivo:
+with open(arquivo_estudantes, 'r') as arquivo:
     for linha in arquivo:
-        chave, valor = linha.strip().split(': ')
-        dados_lidos[chave] = valor
+        linha = linha.strip()
+        if linha.startswith('nome: '):
+            nome = linha[len('nome: '):]
+            estudante_atual = {'nome': nome}
+        elif linha.startswith('idade: '):
+            idade = int(linha[len('idade: '):])
+            estudante_atual['idade'] = idade
+        elif linha.startswith('curso: '):
+            curso = linha[len('curso: '):]
+            estudante_atual['curso'] = curso
+            dados_lidos[nome] = estudante_atual
 
-print(dados_lidos)
+for nome, info in dados_lidos.items():
+    print(f"Nome: {nome}, Idade: {info['idade']}, Curso: {info['curso']}")
